095ea272f2583da66de584afbbbb45da2b7e750f
danho <danho011185@gmail.com>
2024-09-26 16:16:19 +0900

    정단호/2024-09-26/add Forex API

---
+++ src/main/java/com/kb/finance/dto/Forex.java
@@ -0,0 +1,17 @@
+package com.kb.finance.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Forex {
+    private long feno; // primary-key
+    private String forexName;
+    private double forexBasicRate;
+    private double forexBuy;
+    private double forexSell;
+}
