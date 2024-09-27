8a942c43e036129612aea64756a553444b9cc488
danho <danho011185@gmail.com>
2024-09-27 10:08:02 +0900

    정단호/2024-09-27/fix Installment API with RequireParams & fix FinSet.sql (depoit_name -> deposit_name)

--- src/main/java/com/kb/finance/mapper/InstallmentMapper.java
+++ src/main/java/com/kb/finance/mapper/InstallmentMapper.java
@@ -6,5 +6,7 @@ import java.util.List;
 
 public interface InstallmentMapper {
     List<Installment> selectAll();
+    List<Installment> selectSimple();
+    List<Installment> selectCompound();
     Installment selectById(Long ino);
 }
