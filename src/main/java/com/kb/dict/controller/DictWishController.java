92582a422874c03f5ce3e612eaab57f5828b787c
hikhjin <hikhjin@gmail.com>
2024-10-11 15:08:25 +0900

    김현진/241011/add: 단어장 순서 바꾸기 기능 추가

--- src/main/java/com/kb/dict/controller/DictWishController.java
+++ src/main/java/com/kb/dict/controller/DictWishController.java
@@ -1,13 +1,16 @@
 package com.kb.dict.controller;
 
-import com.kb.dict.dto.Dict;
 import com.kb.dict.dto.DictWish;
+import com.kb.dict.dto.DictWishOrder;
+import com.kb.dict.dto.DictWishOrderDTO;
 import com.kb.dict.service.DictWishService;
+import com.kb.member.dto.Member;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +23,8 @@ import java.util.List;
 public class DictWishController {
     private final  DictWishService dictWishService;
     @GetMapping("")
-    public ResponseEntity<List<DictWish>> getList(@RequestParam long uno) {
-        return ResponseEntity.ok(dictWishService.getList(uno));
+    public ResponseEntity<List<DictWish>> getList(@AuthenticationPrincipal Member member) {
+        return ResponseEntity.ok(dictWishService.getList(member.getUno()));
     }
 
     @GetMapping("/{dwno}")
@@ -31,7 +34,7 @@ public class DictWishController {
     }
 
     @PutMapping("/update/{dwno}")
-    public ResponseEntity<?> updateWish(@PathVariable long dwno,@RequestBody DictWish wish) {
+    public ResponseEntity<?> updateWish(@PathVariable long dwno, @RequestBody DictWish wish) {
         wish.setDwno(dwno);
         int rowsUpdated = dictWishService.updateWish(wish);
         if (rowsUpdated > 0) {
@@ -39,5 +42,11 @@ public class DictWishController {
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("업데이트된 행이 없습니다");
         }
+    }
+
+    @PatchMapping("/order")
+    public ResponseEntity<String> updateOrder(@RequestBody DictWishOrderDTO dictWishOrderDTO) {
+        dictWishService.updateWishOrder(dictWishOrderDTO);
+        return ResponseEntity.ok("modify order successful");
     }
 }
--- src/main/java/com/kb/dict/service/DictWishService.java
+++ src/main/java/com/kb/dict/service/DictWishService.java
@@ -2,6 +2,8 @@ package com.kb.dict.service;
 
 import com.kb.dict.dto.Dict;
 import com.kb.dict.dto.DictWish;
+import com.kb.dict.dto.DictWishOrder;
+import com.kb.dict.dto.DictWishOrderDTO;
 import com.kb.dict.mapper.DictWishMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,6 +36,32 @@ public class DictWishService {
     }
     public int updateWish(DictWish wish) {
        return wishMapper.updateWish(wish);
+    }
+
+    public int updateWishOrder(DictWishOrderDTO dictWishOrderDTO) {
+        List<DictWishOrder> wishOrders = dictWishOrderDTO.getDictWishOrders();
+        int result = 0;
+
+        DictWishOrder dictWishOrder1 = wishOrders.get(0);
+        DictWishOrder dictWishOrder2 = wishOrders.get(1);
+
+        result = wishMapper.initWishOrder(dictWishOrder2);
+        if (result != 1) {
+            throw new NoSuchElementException("Init failed for order: " + dictWishOrder2.getDwno());
+        }
+        result = wishMapper.updateWishOrder(dictWishOrder1);
+        if (result != 1) {
+            throw new NoSuchElementException("Update failed for order: " + dictWishOrder1.getDwno());
+        }
+
+        dictWishOrder2.setDwno(-1);
+        System.out.println(dictWishOrder2.getDwno());
+        result = wishMapper.updateWishOrder(dictWishOrder2);
+        if (result != 1) {
+            throw new NoSuchElementException("Update failed for order: " + dictWishOrder2.getDwno());
+        }
+
+        return result;
     }
 
 
